0345f6a77e6e89d0307ea675241c2da243921b2c
Duke Murden <dmurden@gmail.com>
2019-11-24 10:43:45 -0600

    Added prod password

--- get_pass.py
+++ get_pass.py
@@ -3,3 +3,6 @@ import os
 os.system('set_env stuira2')
 pwdSysDvl = os.environ['PWDSYSTEM']
 pwdSys = os.environ['PWDSYS']
+# set_env stpira2
+os.system('set_env stpira2')
+pwdSysPrd = os.environ['PWDSYSTEM']
