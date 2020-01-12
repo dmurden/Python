20e70bd37c7e76b0eb86e657530f81e28293bc66
Duke Murden <dmurden@gmail.com>
2020-01-11 20:39:32 -0600

    More changes

--- convert/convert.py
+++ convert/convert.py
@@ -1,5 +1,5 @@
-cout = open('convert/carynbro_c.html', 'w')
-with open('convert/carynbro.html') as fin:
+cout = open('convert/debrabro_c.html', 'w')
+with open('convert/debrabro.html') as fin:
     line = fin.readline()
     while line:
         cout.write(line + '<br>')
