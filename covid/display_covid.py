5528729b7ed851e7b4150930c1696b4584d3fa06
Duke Murden <dmurden@gmail.com>
2020-05-09 17:03:11 -0500

    Cleaning up the debug code

--- covid/display_covid.py
+++ covid/display_covid.py
@@ -14,7 +14,6 @@ if int(infips) > 999:
     for x in data[1:]:
         if x[0] == infips:
             pyplot.title(x[1] + ' in ' + x[2])
-            print(x[1] + ' in ' + x[2])
             for y in range(4,len(x)):
                 daily.append(int(x[y]))
 elif int(infips) != 0:
@@ -22,7 +21,6 @@ elif int(infips) != 0:
         if x[3] == infips:
             if len(daily) == 0:
                 pyplot.title(x[2])
-                print(x[2])
                 for y in range(4,len(x)):
                     daily.append(int(x[y]))
             else:
@@ -32,17 +30,14 @@ else:
     for x in data[1:]:
         if len(daily) == 0:
             pyplot.title('USA')
-            print('USA')
             for y in range(4,len(x)):
                 daily.append(int(x[y]))
         else:
             for y in range(4,len(x)):
                 daily[y-4] += int(x[y])
-print(daily)
 dataout = [0]
 for y in range(1,len(daily)):
     dataout.append(daily[y] - daily[y-1])
-print(dataout)
 avgout = [0,0,0,0,0,0]
 i = 6
 while i < len(dataout):
